3f593a5d7fee5fff423b968e050f0d6cb4aab9fa
tidilia <tidiliaaa@gmail.com>
2026-04-25 02:05:32 +0500

    api router added

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
+from app.api.router import api_router
 
 app = FastAPI()
 
-@app.get("/health")
-def health():
-    return {"status": "ok"}
-
+app.include_router(api_router, prefix="/api")
